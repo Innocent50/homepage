161513f231d02077184da81d7d70861cdbaf03b9
INNOCENT NZIMENYERA <36258940+Innocent50@users.noreply.github.com>
2023-05-19 10:21:55 +0200

    update leadership field

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -103,29 +103,29 @@ const repos = {
   specificRepos:[],
 };
 
-// Leadership SECTION
-// const leadership = {
-//   show: false,
-//   heading: "Leadership",
-//   message:
-//     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
-//   images: [
-//     { 
-//       img: require("../editable-stuff/profile.png"), 
-//       label: "First slide label", 
-//       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-//     },
-//     { 
-//       img: require("../editable-stuff/profile.png"), 
-//       label: "Second slide label", 
-//       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-//     },
-//   ],
-//   imageSize: {
-//     width:"615",
-//     height:"450"
-//   }
-// };
+//Leadership SECTION
+const leadership = {
+  show: false,
+  heading: "Leadership",
+  message:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+  images: [
+    { 
+      img: require("../editable-stuff/profile.png"), 
+      label: "First slide label", 
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    },
+    { 
+      img: require("../editable-stuff/profile.png"), 
+      label: "Second slide label", 
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    },
+  ],
+  imageSize: {
+    width:"615",
+    height:"450"
+  }
+};
 
 // SKILLS SECTION
 const skills = {
@@ -170,4 +170,4 @@ const getInTouch = {
 //   show: false,
 // };
 
-export { navBar, mainBody, about,education,experiences,repos,skills, getInTouch};
+export { navBar, mainBody, about,education,experiences,repos,skills,leadership,getInTouch};
